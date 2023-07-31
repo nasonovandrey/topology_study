35edf0cb75d8f24e78b4ff479ff6b31fe6adb2f3
nasonovandrey <andrey.m.nasonov@gmail.com>
2023-07-31 13:00:38 +0600

    Add several metrics

--- network_features.py
+++ network_features.py
@@ -1,44 +1,70 @@
 import numpy as np
 import pandas as pd
+import networkx as nx
 
 
+# ---Helpers----------------------------
 def dfs(node, adj_matrix, visited):
-    # Mark the current node as visited
     visited[node] = True
-
-    # Perform DFS on neighbors of the current node
     for neighbor in range(len(adj_matrix)):
         if adj_matrix[node, neighbor] == 1 and not visited[neighbor]:
             dfs(neighbor, adj_matrix, visited)
 
 
-def average_links(adj_matrix):
-    # Step 1: Compute the sum of each row in the adjacency matrix
-    outdegrees = np.sum(adj_matrix, axis=1)
-    # Step 2: Calculate the average number of causal links per node
-    average_links_per_node = np.mean(outdegrees)
-    return average_links_per_node
+# --------------------------------------
 
 
-def density(adj_matrix):
-    num_nodes = adj_matrix.shape[0]
-    num_edges = np.sum(adj_matrix)
+def diameter(G):
+    diameters = []
+    for component in nx.connected_components(G):
+        subgraph = G.subgraph(component)
+        try:
+            diameter = nx.diameter(subgraph)
+            diameters.append(diameter)
+        except nx.NetworkXError:
+            pass
 
-    # Calculate the density of the network
-    density = num_edges / (num_nodes * (num_nodes - 1))
+    max_diameter = max(diameters)
+    return max_diameter
+
+
+def shortest_average_path(G):
+    avg_shortest_paths = []
+    for component in nx.connected_components(G):
+        subgraph = G.subgraph(component)
+        try:
+            avg_shortest_path = nx.average_shortest_path_length(subgraph)
+            avg_shortest_paths.append(avg_shortest_path)
+        except nx.NetworkXError:
+            pass  # Ignore isolated nodes or components with no paths.
+
+    if avg_shortest_paths:
+        avg_path = sum(avg_shortest_paths) / len(avg_shortest_paths)
+        return avg_path
+    else:
+        return None
+
+
+def clustering_coefficient(G):
+    cc = nx.average_clustering(G)
+    return cc
+
+
+def average_links(G):
+    total_links = sum([G.degree(node) for node in G.nodes()])
+    num_nodes = G.number_of_nodes()
+    average_links = total_links / num_nodes
+    return average_links
+
+
+def density(G):
+    num_nodes = G.number_of_nodes()
+    num_edges = G.number_of_edges()
+    density = 2 * num_edges / (num_nodes * (num_nodes - 1))
 
     return density
 
 
-def clusters_count(adj_matrix):
-    num_nodes = len(adj_matrix)
-    visited = [False] * num_nodes
-    num_clusters = 0
-
-    # Perform DFS from each unvisited node
-    for node in range(num_nodes):
-        if not visited[node]:
-            dfs(node, adj_matrix, visited)
-            num_clusters += 1
-
-    return num_clusters
+def num_connected_components(G):
+    num_components = nx.number_connected_components(G)
+    return num_components
